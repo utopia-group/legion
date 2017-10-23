18b2f42bb01a769f9ab8a23e8417c47982567a00
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-24 00:20:22 +0200

    regent: Upate Stencil run script.

--- language/scripts/sbatch_stencil.sh
+++ language/scripts/sbatch_stencil.sh
@@ -18,7 +18,7 @@ for i in 0 1 2 3; do
     for r in 0 1 2 3 4; do
         if [[ ! -f out_"$n"x10_r"$r".log ]]; then
             echo "Running $n""x10_r$r"" ($n = $nx * $ny)..."
-	    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/stencil.spmd10" -nx 40000 -ny 40000 -ntx $(( nx * 2 )) -nty $(( ny * 5 )) -tsteps 100 -tprune 5 -hl:sched -1 -ll:cpu 10 -ll:io 1 -ll:util 1 -ll:dma 2 -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -hl:prof 4 -level legion_prof=2 -logfile prof_"$n"x10_r"$r"_%.log | tee out_"$n"x10_r"$r".log
+	    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/stencil.spmd10" -nx 40000 -ny 40000 -ntx $(( nx * 2 )) -nty $(( ny * 5 )) -tsteps 100 -tprune 5 -hl:sched -1 -ll:cpu 10 -ll:io 1 -ll:util 1 -ll:dma 2 -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -hl:prof 4 -hl:prof_logfile prof_"$n"x10_r"$r"_%.gz | tee out_"$n"x10_r"$r".log
         fi
     done
 done
