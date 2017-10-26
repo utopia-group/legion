165a67c81758769317287b6d0c326f27596275a3
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-25 22:10:34 -0700

    regent: Modernize scripts.

--- language/scripts/sbatch_stencil_notracing_small.sh
+++ language/scripts/sbatch_stencil_notracing_small.sh
@@ -18,7 +18,7 @@ for i in 3 2 1 0; do
     for r in 0 1 2 3 4; do
         if [[ ! -f out_"$n"x10_r"$r".log ]]; then
             echo "Running $n""x10_r$r"" ($n = $nx * $ny)..."
-	    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/stencil.spmd10" -nx 40000 -ny 40000 -ntx $(( nx * 2 )) -nty $(( ny * 5 )) -tsteps 100 -tprune 5 -ll:cpu 10 -ll:io 1 -ll:util 1 -ll:dma 2 -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -lg:no_physical_tracing -hl:prof 4 -hl:prof_logfile prof_"$n"x10_r"$r"_%.gz | tee out_"$n"x10_r"$r".log
+	    srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/stencil.spmd10" -nx 40000 -ny 40000 -ntx $(( nx * 2 )) -nty $(( ny * 5 )) -tsteps 100 -tprune 5 -ll:cpu 10 -ll:io 1 -ll:util 1 -ll:dma 2 -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -lg:no_physical_tracing -lg:prof 4 -lg:prof_logfile prof_"$n"x10_r"$r"_%.gz | tee out_"$n"x10_r"$r".log
         fi
     done
 done
