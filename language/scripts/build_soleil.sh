7e31a819e520c502d0efdca63f7036a5953ffc20
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-27 22:50:50 +0200

    regent: More typos.

--- language/scripts/build_soleil.sh
+++ language/scripts/build_soleil.sh
@@ -14,7 +14,7 @@ for i in 0 1 2 3 4 5 6 7 8; do
     nz=$(( 2 ** ((i+0)/3) ))
 
     for c in 10; do
-        time USE_HDF=0 TERRA_PATH=$root_dir/../liszt-legion/include/?.t SAVEOBJ=1 OBJNAME=taylor-512x512x256-dop"$n" $root_dir/../regent.py soleil-x/src/soleil-x.t -i soleil-x/testcases/taylor_green_vortex/taylor_green_vortex_$(( 512 * ny))_$(( 512 * nz ))_$(( 256 * nx )).lua -fopenmp 1 -fflow-spmd 1 -fflow-spmd-shardsize 1 -fparallelize-dop "$nx","$ny","$nz" -fcuda 0 &
+        time USE_HDF=0 TERRA_PATH=$root_dir/../liszt-legion/include/?.t SAVEOBJ=1 OBJNAME=taylor-512x512x256-dop"$n" $root_dir/../regent.py $root_dir/../soleil-x/src/soleil-x.t -i $root_dir/../soleil-x/testcases/taylor_green_vortex/taylor_green_vortex_$(( 512 * ny))_$(( 512 * nz ))_$(( 256 * nx )).lua -fopenmp 1 -fflow-spmd 1 -fflow-spmd-shardsize 1 -fparallelize-dop "$nx","$ny","$nz" -fcuda 0 &
         sleep 3
     done
 done
