29439f5c5e2ab1c3ff03a04f91f87934828bb1e7
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-27 21:15:01 +0200

    regent: Full suite of Pennant scripts.

---
+++ language/scripts/sbatch_pennant_notracing_small.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --nodes=8
+#SBATCH --constraint=gpu
+#SBATCH --time=00:30:00
+#SBATCH --mail-type=ALL
+
+root_dir="$PWD"
+
+export LD_LIBRARY_PATH="$PWD"
+
+if [[ ! -d notracing ]]; then mkdir notracing; fi
+pushd notracing
+
+for n in 8 4 2 1; do
+    for r in 0 1 2 3 4; do
+        if [[ ! -f out_"$n"x10_r"$r".log ]]; then
+            echo "Running $n""x10_r$r""..."
+            srun -n $n -N $n --ntasks-per-node 1 --cpu_bind none /lib64/ld-linux-x86-64.so.2 "$root_dir/pennant.spmd10" "$root_dir/pennant.tests/leblanc_long4x30/leblanc.pnt" -npieces $(( $n * 10 )) -numpcx 1 -numpcy $(( $n * 10 )) -seq_init 0 -par_init 1 -print_ts 1 -prune 5 -ll:cpu 10 -ll:io 1 -ll:util 1 -ll:dma 1 -ll:csize 30000 -ll:rsize 512 -ll:gsize 0 -lg:no_physical_tracing | tee out_"$n"x10_r"$r".log
+            # -lg:prof 4 -lg:prof_logfile prof_"$n"x10_r"$r"_%.gz
+        fi
+    done
+done
+
+popd
