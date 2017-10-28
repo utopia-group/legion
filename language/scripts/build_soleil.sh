16a1f3aea046a02eb1b479a57baf49fc912f5d34
Elliott Slaughter <slaughter@cs.stanford.edu>
2017-10-28 07:48:49 +0200

    regent: Switch to smaller problem size for Soleil.

--- language/scripts/build_soleil.sh
+++ language/scripts/build_soleil.sh
@@ -13,7 +13,7 @@ for i in 0 1 2 3 4 5 6 7 8 9 10; do
     ny=$(( 2 ** ((i+1)/3) ))
     nz=$(( 2 ** ((i+0)/3) ))
 
-    time USE_HDF=0 TERRA_PATH=$root_dir/../liszt-legion/include/?.t SAVEOBJ=1 OBJNAME=taylor-512x512x256-dop"$n" $root_dir/../regent.py $root_dir/../soleil-x/src/soleil-x.t -i $root_dir/../soleil-x/testcases/taylor_green_vortex/taylor_green_vortex_512_512_256.lua -fopenmp 1 -fflow-spmd 1 -fflow-spmd-shardsize 1 -fparallelize-dop "$nx","$ny","$nz" -fcuda 0 &
+    time USE_HDF=0 TERRA_PATH=$root_dir/../liszt-legion/include/?.t SAVEOBJ=1 OBJNAME=taylor-256x256x256-dop"$n" $root_dir/../regent.py $root_dir/../soleil-x/src/soleil-x.t -i $root_dir/../soleil-x/testcases/taylor_green_vortex/taylor_green_vortex_256_256_256.lua -fopenmp 1 -fflow-spmd 1 -fflow-spmd-shardsize 1 -fparallelize-dop "$nx","$ny","$nz" -fcuda 0 &
     sleep 3
 done
 
